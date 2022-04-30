4014df42b3dd4c84651b62c0a7970e65b6319422
Earl Amante <earlamante@w3bkit.com>
2022-04-30 13:32:27 +0800

    fix for 1&1 server

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>App Name - @yield('title')</title>
+    <title>{{ config('app.name') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
